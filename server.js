2a866a14f4e7121cc80366f3b313c3959c9b3692
sharjeelafzaal123 <muhammad.sharjeel@codefrenetics.com>
2023-07-26 19:15:26 +0500

    adding engines script

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(express.json({ extended: false }));
 // app.get("/", (req, res) => res.send("API Running"));
 
 // Defining Routes
-
 app.use(`/api/users`, require("./routes/api/users"));
 app.use(`/api/auth`, require("./routes/api/auth"));
 app.use(`/api/profile`, require("./routes/api/profile"));
